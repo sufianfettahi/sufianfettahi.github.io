d577656e474f04ff0d9230771aeb0d68d1958e29
sufianfettahi <105921720+sufianfettahi@users.noreply.github.com>
2023-09-14 20:13:33 +0200

    creando sitio web

---
+++ js/scripts - copia.js
@@ -0,0 +1,53 @@
+ID_VIDEO = "reproduccion";
+ID_CAJA_VIDEO = "caja-video";
+
+function googleTranslateElementInit() {
+	new google.translate.TranslateElement({pageLanguage: 'es', includedLanguages: 'ar,ca,eu,gl,en,fr,it,pt,de', layout: google.translate.TranslateElement.InlineLayout.SIMPLE, gaTrack: true},
+	'google_translate_element');
+}
+
+function borrado()
+{
+	alert("HAS BORRADO TODO.");
+}
+
+function enviado()
+{
+	alert("SU CONSULTA HA SIDO ENVIADA CON EXITO, en breve rebira una respuesta. GRACIAS!");
+	limpiarFormulario();
+}
+
+function limpiarFormulario() {
+    document.getElementById("miForm").reset();
+  }
+
+function iniciar() {
+    reproduccion = getById(ID_VIDEO);
+    cajaVideo = getById(ID_CAJA_VIDEO);
+	alert("Soufiane El Fettahi, 2ºDAM.");
+}
+
+function getById(element_id) {
+    return document.getElementById(element_id);
+}
+
+function playVideoTiempo() {
+    reproduccion.play();
+}
+
+function pauseVideoTiempo() {
+    reproduccion.pause();
+}
+
+function stopVideoTiempo() {
+    reproduccion.currentTime = 0;
+    pauseVideoTiempo();
+}
+
+function changeSoundState() {
+    if (reproduccion.muted) {
+        reproduccion.muted = false;
+    } else {
+        reproduccion.muted = true;
+    }
+}
